0ece0274c6918f9b0d4c4f934145c9dcd76b2dc2
Hussain1402 <141639385+Hussain1402@users.noreply.github.com>
2024-07-23 17:57:12 +0530

    Create Possitive_Negative.cpp

---
+++ Possitive_Negative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    if (x>=0)
+    {
+        cout<<"It is a positive number.";
+    }
+    else
+    {
+        cout<<"It is a negative number.";
+    }
+}
+/*Enter a number: -54
+It is a negative number.*/
